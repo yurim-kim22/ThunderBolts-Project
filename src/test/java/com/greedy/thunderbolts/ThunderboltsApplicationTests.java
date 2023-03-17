5fb0f32f6761ba5432f1ce1309247cc187762c5a
병오 강 <byoungoh.kang@gmail.com>
2023-03-17 09:28:20 +0900

    commit

---
+++ src/test/java/com/greedy/thunderbolts/ThunderboltsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greedy.thunderbolts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThunderboltsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
