1ff4ac528628acf8cfd6722d6c2fb99fb9f91704
yujunglove <dbwjdeor1234562@gmail.com>
2023-03-20 18:09:31 +0900

    commit

--- src/main/java/com/greedy/thunderbolts/controller/buselllist/BuySellListController.java
+++ src/main/java/com/greedy/thunderbolts/controller/buselllist/BuySellListController.java
@@ -2,13 +2,44 @@ package com.greedy.thunderbolts.controller.buselllist;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/list")
 public class BuySellListController {
 	
-	@GetMapping("/list/normalBuy")
+	@GetMapping("/normalBuy")
 	public String normalBuy(){
-		return "common/productList/normalBuy";
+		return "productList/normalBuy";
+		
+	}
+	@GetMapping("/normalSell")
+	public String normalSell(){
+		return "productList/normalSell";
+		
+	}
+	
+	@GetMapping("/oneSizeBuying")
+	public String oneSizeBuying(){
+		return "productList/oneSizeBuying";
+		
+	}
+	
+	@GetMapping("/oneSizeSelling")
+	public String oneSizeSelling(){
+		return "productList/oneSizeSelling";
+		
+	}
+	
+	@GetMapping("/buyAgree")
+	public String buyAgree(){
+		return "agreeAtc/buyAgree";
+		
+	}
+	
+	@GetMapping("/sellAgree")
+	public String sellAgree(){
+		return "agreeAtc/sellAgree";
 		
 	}
 
