9d920cb5d5ae66f7ea679dee4c07ddab3c7208c5
cloverpig <cloverpig@ByoungsNotebook>
2023-03-21 20:38:53 +0900

    2023/03/21 메인베너 자동 변경 완료

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,14 +9,60 @@ if (document.getElementById('bannericon')) {
 	$bannericon.addEventListener('click', e => {
 		const $bannerbutton = document.getElementsByClassName('button')
 		const $button = Array.from($bannerbutton);
-
+		const $buttonli = Array.from($bannericon.children);
+	
 		if (e.target.matches('.bannerbutton, .button')) {
 			$button.forEach(c => c.className = 'bannerbutton');
 			e.target.className = 'button';
 		}
+		
+		$buttonli.forEach((li,index)=>{
+			if(e.target == li){
+			$bannericon.previousElementSibling.style.backgroundImage="url(/upload/image/main/banner/mainbanner"+(index+1)+".png)";
+			}
+		})
+		
 
 	});
 };
+
+
+
+let countNum = 0;
+function bannerNext(){
+	
+	const $bannericon = document.getElementById('bannericon');	
+	const $buttonli = Array.from($bannericon.children);
+	for (let i =0 ; i < $buttonli.length; i++){
+	
+		if(i === countNum){
+		$bannericon.previousElementSibling.style.backgroundImage="url(/upload/image/main/banner/mainbanner"+(countNum+1)+".png)";
+		$buttonli[countNum].className = 'button';
+		
+		} else {
+			
+			$buttonli[i].className = 'bannerbutton';
+		}
+	}
+		
+	if(countNum < $buttonli.length){
+		countNum++; 
+		if(countNum === $buttonli.length ){
+			countNum = 0;	
+		}
+	}
+	
+	setTimeout(bannerNext, 2000);
+}
+bannerNext();
+
+
+
+
+
+
+
+
 /* 관심상품 클릭시 하트 이미지 변경*/
 $(".eheart").on('click', function(e) {
 	e.target.classList.toggle('heart')
@@ -78,3 +124,6 @@ infinite: true,
     nextArrow: "<i class='nextimg'></i>", 
 	prevArrow: "<i class='preimg'></i>"
 });
+
+
+
