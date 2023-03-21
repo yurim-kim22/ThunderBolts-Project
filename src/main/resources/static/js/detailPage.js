da5624f78811ad3c476a40df75c5d12511625712
cloverpig <cloverpig@ByoungsNotebook>
2023-03-21 12:27:12 +0900

    2023/03/21 상세페이지수정, 메인페이지 슬라이드라이브러리 진행중

--- src/main/resources/static/js/detailPage.js
+++ src/main/resources/static/js/detailPage.js
@@ -1,6 +1,46 @@
 
 $(document).ready(function(){}); 
 
+if($(".eheart") != null){
 $(".eheart").on('click', function(e){
     e.target.classList.toggle('heart')
 });
+
+}
+
+if ($(".downImg") != null) {
+	$(".downImg").on('click', function(e) {
+		e.target.classList.toggle('upImg');
+		if (e.target.parentElement.className == "deliveryNoticeTitle") {
+			if ($(".deliveryNoticeContent").css("display") == "none") {
+				$(".deliveryNoticeTitle").css({"font-weight": "600", "border-bottom": "2px solid black"});
+				$(".deliveryNoticeContent").css("display", "block");
+			} else {
+				$(".deliveryNoticeTitle").css({"font-weight": "500", "border-bottom": "1px solid gray"});
+				$(".deliveryNoticeContent").css("display", "none");
+			}
+		}
+		
+		if (e.target.parentElement.className == "checkNoticeTitle") {
+			if ($(".checkNoticeContent").css("display") == "none") {
+				$(".checkNoticeTitle").css({"font-weight": "600", "border-bottom": "2px solid black"});
+				$(".checkNoticeContent").css("display", "block");
+			} else {
+				$(".checkNoticeTitle").css({"font-weight": "500", "border-bottom": "1px solid gray"});
+				$(".checkNoticeContent").css("display", "none");
+			}
+		}
+		
+		if (e.target.parentElement.className == "buyNoticeTitle") {
+			if ($(".buyNoticeContent").css("display") == "none") {
+				$(".buyNoticeTitle").css({"font-weight": "600", "border-bottom": "2px solid black"});
+				$(".buyNoticeContent").css("display", "block");
+			} else {
+				$(".buyNoticeTitle").css({"font-weight": "500", "border-bottom": "1px solid gray"});
+				$(".buyNoticeContent").css("display", "none");
+			}
+		}
+		
+	});
+
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,28 +1,36 @@
-if (document.getElementById('bannericon')){
-/* 메인 베너 하다 버튼 이미지 클릭시 변경*/
-const $bannericon = document.getElementById('bannericon');
-console.log($bannericon);
-$bannericon.addEventListener('click', e =>{
-    const $bannerbutton = document.getElementsByClassName('button')
-    const $button = Array.from($bannerbutton);
-        if(e.target.matches('.bannerbutton button, .button button')){
-        $button.forEach(c => c.className='bannerbutton');
-        e.target.parentElement.className = 'button';
-    }
-  
-});
+$(document).ready(function() { });
+
+
+
+if (document.getElementById('bannericon')) {
+	/* 메인 베너 하다 버튼 이미지 클릭시 변경*/
+	const $bannericon = document.getElementById('bannericon');
+
+	$bannericon.addEventListener('click', e => {
+		const $bannerbutton = document.getElementsByClassName('button')
+		const $button = Array.from($bannerbutton);
+
+		if (e.target.matches('.bannerbutton, .button')) {
+			$button.forEach(c => c.className = 'bannerbutton');
+			e.target.className = 'button';
+		}
+
+	});
 };
 /* 관심상품 클릭시 하트 이미지 변경*/
-
-$(document).ready(function(){}); 
-
-$(".eheart").on('click', function(e){
-    e.target.classList.toggle('heart')
+$(".eheart").on('click', function(e) {
+	e.target.classList.toggle('heart')
 });
 
 /* 슬라이드 클릭시 변경 (작성중) */
-$(".nextimg").on('click',function(){
-    
-    console.log($('#producthot'))    
-    
+console.log($('#producthot'));
+
+$('#producthot').Slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3
 });
+
+
+
+
