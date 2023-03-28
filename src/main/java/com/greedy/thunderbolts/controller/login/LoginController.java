34ced92992c2732b1a8e9025abc8a3819550a244
yujunglove <dbwjdeor1234562@gmail.com>
2023-03-28 17:33:18 +0900

    💙MERGE 0328

--- src/main/java/com/greedy/thunderbolts/controller/login/LoginController.java
+++ src/main/java/com/greedy/thunderbolts/controller/login/LoginController.java
@@ -145,7 +145,7 @@ public class LoginController {
 		} else {
 			rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("member.searchPwerror"));
 		}
-		
+		 
 		return "redirect:/login/main";
 	}
 	
